448438d74e63deac463880c261090f2894d8e098
Sagar-G1 <sagar.g1@ibm.com>
2022-07-22 14:26:20 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sample-ios-automate-build',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
